20ac1f1ba2c947754aa730c469f053cf56065064
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-10-18 12:09:09 -0300

    ci: setup ci actions

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
 import "react-native-gesture-handler";
+import React, { useEffect } from "react";
+import * as Updates from "expo-updates";
 import Banky from "./src";
 
 export default function App() {
+  useEffect(() => {
+    async function updateApp() {
+      if (process.env.NODE_ENV === "development") {
+        return;
+      }
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+    updateApp();
+  }, []);
   return <Banky />;
 }
